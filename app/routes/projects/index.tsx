da9716744ae5d7145ea4755ccf0c89209e953324
Amer Melhem <amermelhem.99@icloud.com>
2025-09-11 19:51:58 +0300

    pagination

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,6 +1,8 @@
 import type { Route } from "./+types/index";
 import type { Project } from "~/types";
 import ProjectCard from "~/components/projectCard";
+import { useState } from "react";
+import Pagination from "~/components/pagination";
 
 export async function loader({
   request,
@@ -13,16 +15,30 @@ export async function loader({
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
   const { projects } = loaderData as { projects: Project[] };
 
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const projectsPerPage = 2;
+  const totalPages = Math.ceil(projects.length / projectsPerPage);
+
+  const indexOfLast = currentPage * projectsPerPage;
+  const indexOfFirst = indexOfLast - projectsPerPage;
+  const currentProjects = projects.slice(indexOfFirst, indexOfLast);
+
   return (
     <>
       <h2 className="font-bold mb-8 text-3xl text-center text-white">
         my projects
       </h2>
       <div className="grid gap-6 sm:grid-cols-2 items-stretch">
-        {projects.map((project) => (
+        {currentProjects.map((project) => (
           <ProjectCard project={project} key={project.id} />
         ))}
       </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={setCurrentPage}
+      />
     </>
   );
 };
