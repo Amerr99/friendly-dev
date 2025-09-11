7125f54b5722eb3fd97a0e45ffc488e07e151799
Amer Melhem <amermelhem.99@icloud.com>
2025-09-11 18:05:45 +0300

    loaders and create a project type

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,20 +1,28 @@
 import type { Route } from "./+types/index";
+import type { Project } from "~/types";
+import ProjectCard from "~/components/projectCard";
 
-export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
   const res = await fetch("http://localhost:8000/projects");
   const data = await res.json();
   return { projects: data };
 }
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
-  const { projects } = loaderData;
-  console.log(projects);
+  const { projects } = loaderData as { projects: Project[] };
 
   return (
     <>
       <h2 className="font-bold mb-8 text-3xl text-center text-white">
         my projects
       </h2>
+      <div className="grid gap-6 sm:grid-cols-2 items-stretch">
+        {projects.map((project) => (
+          <ProjectCard project={project} key={project.id} />
+        ))}
+      </div>
     </>
   );
 };
